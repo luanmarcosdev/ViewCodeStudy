552119f8d4b9bd99c867a85ea66e1a621b65ea1e
Luan <luanmarcosdev@gmail.com>
2024-03-05 17:36:31 -0300

    create a label

--- LoginViewCode-curso/LoginViewCode-curso/ViewController.swift
+++ LoginViewCode-curso/LoginViewCode-curso/ViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // criando o Label
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
 
+    //adicionando o label
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        self.view.backgroundColor = .blue
+        self.view.addSubview(self.loginLabel)
+        self.setUpConstraints()
+    }
+    
+    //setando constrains
+    private func setUpConstraints(){
+        NSLayoutConstraint.activate([
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.loginLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 
 }
 
