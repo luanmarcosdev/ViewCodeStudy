c2bca07cd685e49e45a1dc1c24d306d06a8b8630
Luan <luanmarcosdev@gmail.com>
2024-03-06 13:43:17 -0300

    organizing project

---
+++ LoginViewCode-curso/LoginViewCode-curso/Login/Screen/LoginScreen.swift
@@ -0,0 +1,38 @@
+//
+//  LoginScreen.swift
+//  LoginViewCode-curso
+//
+//  Created by Luan Arruda on 06/03/24.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(self.loginLabel)
+        self.setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpConstraints(){
+        NSLayoutConstraint.activate([
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.loginLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+
+}
---
+++ LoginViewCode-curso/LoginViewCode-curso/Login/ViewController/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  LoginViewCode-curso
+//
+//  Created by Luan Arruda on 05/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var loginScreen: LoginScreen?
+    
+    override func loadView() {
+        self.loginScreen = LoginScreen()
+        self.view = self.loginScreen
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
+
