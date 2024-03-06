c4f7e4c440ed3f9a4667c3a040d3029523de14dd
Luan <luanmarcosdev@gmail.com>
2024-03-06 14:51:39 -0300

    creating logo image view and seting constraints

--- LoginViewCode-curso/LoginViewCode-curso/Login/Screen/LoginScreen.swift
+++ LoginViewCode-curso/LoginViewCode-curso/Login/Screen/LoginScreen.swift
@@ -18,9 +18,20 @@ class LoginScreen: UIView {
         return label
     }()
     
+    lazy var logoAppImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "logo")
+        image.tintColor = .green
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = UIColor(red: 24/255, green: 117/255, blue: 104/255, alpha: 1.0)
         self.addSubview(self.loginLabel)
+        self.addSubview(self.logoAppImageView)
         self.setUpConstraints()
     }
     
@@ -30,8 +41,15 @@ class LoginScreen: UIView {
     
     private func setUpConstraints(){
         NSLayoutConstraint.activate([
+            
+            self.loginLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
             self.loginLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.loginLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            
+            self.logoAppImageView.topAnchor.constraint(equalTo: self.loginLabel.bottomAnchor, constant: 20),
+            self.logoAppImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 60),
+            self.logoAppImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -60),
+            self.logoAppImageView.heightAnchor.constraint(equalToConstant: 200)
+        
         ])
     }
 
--- LoginViewCode-curso/LoginViewCode-curso/Login/ViewController/ViewController.swift
+++ LoginViewCode-curso/LoginViewCode-curso/Login/ViewController/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
     }
  
     override func viewWillAppear(_ animated: Bool) {
